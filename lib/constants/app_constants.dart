0a49e902debd9d7dc541fc413274851220ab151f
Dawit <dwtmekonnen123@gmail.com>
2021-11-12 11:08:25 +0300

    first commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+final Color mainHexColor = Color.fromRGBO(38, 38, 38, 0.4);
+final Color accentHexColor = Colors.blue;
---
+++ lib/widgets/left_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../constants/app_constants.dart';
+
+class LeftBar extends StatelessWidget {
+  final double barWidth;
+  LeftBar({this.barWidth = 0});
+  // const LeftBar({Key key, @required this.barWidth}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+              color: accentHexColor),
+        )
+      ],
+    );
+  }
+}
